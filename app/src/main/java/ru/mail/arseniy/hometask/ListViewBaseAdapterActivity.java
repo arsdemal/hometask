04c112e91e6ded97f8c4d05038a70e0bc20a322d
Arseniy <arsdemal@gmail.com>
2016-06-29 23:46:09 +0300

    BaseAdapter

--- app/src/main/java/ru/mail/arseniy/hometask/ListViewBaseAdapterActivity.java
+++ app/src/main/java/ru/mail/arseniy/hometask/ListViewBaseAdapterActivity.java
@@ -15,41 +15,90 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Objects;
 
 public class ListViewBaseAdapterActivity extends Activity {
 
-    public String getHundred(String number) {
+    public String getHundred(String number, Boolean flag) {
 
         Resources res = getResources();
         String[] hundreds = res.getStringArray(R.array.hundred);
         String[] decades = res.getStringArray(R.array.decade);
         String[] units = res.getStringArray(R.array.unit);
+        if (flag==Boolean.TRUE) {
+            System.arraycopy(res.getStringArray(R.array.thousand_unit),0,units,1,2);
+        }
         String[] teen = res.getStringArray(R.array.teen);
         String result = "";
 
-        if (number.length() > 2) {
-            result += hundreds[Character.digit(number.charAt(0), 10)-1] + " ";
-            if (Character.digit(number.charAt(1), 10) == 1) {
-                result += teen[Character.digit(number.charAt(2), 10)] + " ";
-            } else {
-                if (number.charAt(1) != '0')
-                    result += decades[Character.digit(number.charAt(1)-2, 10)] + " ";
-                if (number.charAt(2) != '0')
-                    result += units[Character.digit(number.charAt(2),10)] + " ";
-            }
-        } else {
-            if (number.length() > 1) {
-                if (number.charAt(0) == '1') {
-                    result += teen[Character.digit(number.charAt(1), 10)] + " ";
+        number = number.startsWith("000") ? "" : number;
+        number = number.startsWith("00") ? number.substring(2) : number;
+        number = number.startsWith("0") ? number.substring(1) : number;
+
+        if (!number.equals("")) {
+            if (number.length() > 2) {
+                result += hundreds[Character.digit(number.charAt(0), 10) - 1] + " ";
+                if (Character.digit(number.charAt(1), 10) == 1) {
+                    result += teen[Character.digit(number.charAt(2), 10)] + " ";
                 } else {
-                    if (number.charAt(0) != '0')
-                        result += decades[Character.digit(number.charAt(0)-2, 10)] + " ";
                     if (number.charAt(1) != '0')
-                        result += units[Character.digit(number.charAt(1),10)] + " ";
+                        result += decades[Character.digit(number.charAt(1) - 2, 10)] + " ";
+                    if (number.charAt(2) != '0')
+                        result += units[Character.digit(number.charAt(2), 10)] + " ";
                 }
             } else {
-                result += units[Character.digit(number.charAt(0),10)] + " ";
+                if (number.length() > 1) {
+                    if (number.charAt(0) == '1') {
+                        result += teen[Character.digit(number.charAt(1), 10)] + " ";
+                    } else {
+                        if (number.charAt(0) != '0')
+                            result += decades[Character.digit(number.charAt(0) - 2, 10)] + " ";
+                        if (number.charAt(1) != '0')
+                            result += units[Character.digit(number.charAt(1), 10)] + " ";
+                    }
+                } else {
+                    result += units[Character.digit(number.charAt(0), 10)] + " ";
+                }
             }
+        }
+
+        return result;
+    }
+
+    public String getThousand(String number) {
+        String result = "";
+        Integer len = number.length();
+        String[] thousand = getResources().getStringArray(R.array.thousand);
+
+        result += getHundred(number,Boolean.TRUE);
+        if (len > 1) {
+            if (number.charAt(len - 2) == '1') {
+                result += thousand[2] + " ";
+            }
+        } else {
+            Character ch = number.charAt(len-1);
+            if (ch == '1') {
+                result += thousand[0] + " ";
+            } else if (ch >= '2' && ch <= '4') {
+                result += thousand[1] + " ";
+            } else {
+                result += thousand[2] + " ";
+            }
+        }
+
+        return result;
+    }
+
+    public String getNumber(String number) {
+
+        Integer len = number.length();
+        String result = "";
+
+        if (len > 3) {
+            result += getThousand(number.substring(0, len - 3));
+            result += getHundred(number.substring(len-3,len),Boolean.FALSE);
+        } else {
+            result += getHundred(number,Boolean.FALSE);
         }
 
         return result;
@@ -62,8 +111,8 @@ public class ListViewBaseAdapterActivity extends Activity {
         ListView lv = (ListView) findViewById(R.id.listview);
 
         ArrayList<String> numbers = new ArrayList<>();
-        for (Integer i = 1; i<=999; i++) {
-            numbers.add(getHundred(i.toString()));
+        for (Integer i = 1; i<=100999; i++) {
+            numbers.add(getNumber(i.toString()));
         }
         //String[] numbers = getResources().getStringArray(R.array.numbers);
         lv.setAdapter(new MyAdapter(this, numbers.toArray(new String[0])));
